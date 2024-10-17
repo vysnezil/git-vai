0b541f75f05c9c2f1162d3401fd77aed361edcc5
vysnezil <mmamma151@gmail.com>
2024-10-17 22:41:59 +0200

    Add mockup repository page

--- src/navbar.svelte
+++ src/navbar.svelte
@@ -1,30 +1,30 @@
 <style lang="postcss">
-    nav {
-        max-height: 20%;
-    }
-    .nav-entry {
-        @apply text-sky-200 hover:text-white;
-    }
+	nav {
+		max-height: 20%;
+	}
+	.nav-entry {
+		@apply text-sky-200 hover:text-white;
+	}
 </style>
 
 <nav class="flex justify-between flex-wrap bg-sky-600 p-4 px-6">
-    <div class="flex flex-wrap items-center gap-8">
-        <div class="flex items-center text-white">
-            <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-            <span class="font-semibold text-xl">
+	<div class="flex flex-wrap items-center gap-8">
+		<div class="flex items-center text-white">
+			<img class="fill-current h-8 w-8 mr-2" width="54" height="54" src="https://upload.wikimedia.org/wikipedia/en/b/b2/Hu_Tao_%28Genshin_Impact%29.png" alt="">
+			<span class="font-semibold text-xl">
                 <a href="/">GITvai</a>
             </span>
-        </div>
-        <div class="flex flex-wrap gap-3">
-            <a href="/page" class="nav-entry">
-                Page1
-            </a>
-            <a href="/page2" class="nav-entry">
-                Page2
-            </a>
-        </div>
-    </div>
-    <div class="flex items-center">
-        <a href="/" class="font-medium text-white hover:text-sky-100">User1</a>
-    </div>
+		</div>
+		<div class="flex flex-wrap gap-3">
+			<a href="/page" class="nav-entry">
+				Page1
+			</a>
+			<a href="/repository" class="nav-entry">
+				Page2
+			</a>
+		</div>
+	</div>
+	<div class="flex items-center">
+		<a href="/" class="font-medium text-white hover:text-sky-100">User1</a>
+	</div>
 </nav>
---
+++ src/routes/repository/+page.svelte
@@ -0,0 +1,96 @@
+<style>
+	.con {
+			margin-top: 20vh;
+	}
+</style>
+
+<base href="/" />
+<div class="con flex justify-center items-start h-full">
+<div class="w-full max-w-3xl flex flex-col justify-center items-left m-5 gap-2">
+	<div class="flex items-center">
+		<div class="my-4 text-3xl text-gray-500"><a href="/">user/</a></div>
+		<div class="text-3xl">
+			 repo
+		</div>
+
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"></path>
+		</svg>
+
+		<div class="flex-grow"></div>
+	</div>
+
+	<div class="flex justify-between items-center w-full">
+		<div>
+			<button class="text-white bg-sky-600 hover:bg-sky-800 focus:ring-2 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">
+				<svg class="w-4 h-4 me-2" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+					<path d="M80 104a24 24 0 1 0 0-48 24 24 0 1 0 0 48zm80-24c0 32.8-19.7 61-48 73.3v87.8c18.8-10.9 40.7-17.1 64-17.1h96c35.3 0 64-28.7 64-64v-6.7C307.7 141 288 112.8 288 80c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V160c0 70.7-57.3 128-128 128H176c-35.3 0-64 28.7-64 64v6.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3V352 153.3C19.7 141 0 112.8 0 80C0 35.8 35.8 0 80 0s80 35.8 80 80zm232 0a24 24 0 1 0 -48 0 24 24 0 1 0 48 0zM80 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"></path>
+				</svg>
+				 master
+				<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"></path>
+				</svg>
+			</button>
+		</div>
+		<div class="flex-grow ml-4 text-sm text-gray-500 hover:text-gray-600">
+			<a href="/"> commit </a>
+		</div>
+		<div class="flex gap-2">
+			<button class="border border-gray-600 flex items-center text-gray-700 bg-transparent hover:bg-slate-400 rounded-lg py-2 px-3 text-sm" type="button">
+				<svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"></path>
+					<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+				</svg>
+			</button>
+			<button class="btn" type="button">
+				<svg class="w-5 h-5 me-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"></path>
+				</svg>
+				Download
+				<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"></path>
+				</svg>
+			</button>
+		</div>
+	</div>
+
+	<div class="rounded-lg shadow bg-slate-50 w-full p-1">
+		<table class="w-full text-gray-600 text-sm table-auto">
+			<tbody>
+				<tr class="border-b h-10 hover:bg-gray-100">
+					<td>
+						<a href="/" class="flex flex-center items-center mx-3 gap-1">
+      	         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"></path>
+					</svg>
+						<div class="w-200">directory</div>
+						</a>
+					</td>
+					<td class="text-gray-400 hover:text-gray-500">
+						<a href="/">Add directory</a>
+					</td>
+					<td class="text-gray-400 hover:text-gray-500 text-right pr-3">
+						17.10.2024 15:47
+					</td>
+				</tr>
+				<tr class="border-b h-10 hover:bg-gray-100">
+					<td>
+						<a href="/" class="flex flex-center items-center mx-3 gap-1">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"></path>
+							</svg>
+							<div class="w-200">file</div>
+						</a>
+					</td>
+					<td class="text-gray-400 hover:text-gray-500">
+						<a href="/">Add file</a>
+					</td>
+					<td class="text-gray-400 hover:text-gray-500 text-right pr-3">
+						17.10.2024 14:34
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+</div>
